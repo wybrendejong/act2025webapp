f0e5b9df53e07398235273f94056964630652cbb
JrmWrm <jerom.bolck@outlook.com>
2025-07-01 13:44:54 +0200

    Implemented accuracy metrics in UI

--- src/classification.py
+++ src/classification.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 from imblearn.ensemble import BalancedRandomForestClassifier
@@ -23,11 +23,11 @@ def train_classifier(gdf, clf_treshold=0):
         clf_treshold (int default 0): Treshold to use for splitting data in binary classes.
         
     Returns:
-        BalancedRandomForestClassifier: Fitted balanced random forest model.
+        brf, precision, recall (BalancedRandomForestClassifier, float, float): Fitted balanced random forest model, precison score, recall score.
     """
     # Create binary target (y): 1 if more rat sightings than threshold, 0 otherwise
-    y = gdf['TOTAAL'].apply(lambda x: 1 if pd.notna(x) and x > clf_treshold else 0)
     X = gdf[features]
+    y = gdf['TOTAAL'].apply(lambda x: 1 if pd.notna(x) and x > clf_treshold else 0)
 
     # Split into train/test
     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=16)
@@ -37,15 +37,17 @@ def train_classifier(gdf, clf_treshold=0):
     brf.fit(X_train, y_train)
 
     # Predict and evaluate
-    y_pred = (brf.predict_proba(X_test)[:, 1] > 0.8).astype(int)
+    y_pred = brf.predict(X_test)
 
-    print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
+
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
 
     # After evaluation retrain the model on all data, for better performance
     brf.fit(X, y)
 
-    return brf
+    return brf, precision, recall
 
 
 def predict_probabilities(gdf, clf):
@@ -86,7 +88,6 @@ def predict_probabilities(gdf, clf):
     # Predict probabilities
     X_pred = next_month_df[features]
     next_month_df['prob_positive'] = clf.predict_proba(X_pred)[:, 1]
-
-    # GeoDataFrame
     next_month_gdf = gpd.GeoDataFrame(next_month_df, geometry='geometry')
+
     return next_month_gdf
--- src/webapp.py
+++ src/webapp.py
@@ -22,76 +22,133 @@ import base64
 
 from src import mapping, data_loading, classification
 
+# Data paths
 pc_gpkg_path = "data/cbs_pc4_2024_v1.gpkg"
 map_html_path = "./map/map.html"
 rat_gdf_path = "./data/rat_gdf.gpkg"
 rat_proba_gdf_path = "./data/rat_proba_gdf.gpkg"
+metrics_path = "./data/metrics.json"
 
+# Predefined texts
+recall_label_text = 'Percentage of areas that actually have rats and are predicted to have rats: '
+precision_label_text = 'Percentage of areas predicted to have rats that actually have rats: '
+accuracy_info_text = """
+    The model that predicts the probabilities in the map was trained and evaluated on the rat monitor data that was uploaded
+    to this website. The percentages shown above represent the accuracy of the model. 20% of the data was not used in training, 
+    but was used to calculate these accuracy metrics. The model makes predictions for this subset of the data which is then 
+    compared to the actual numbers.\n\n 
+    These metrics describe how well the model performs in detecting areas with rats present. 
+    The first metric evaluates the ability of the model to find all areas that actually have rats. It is assesed by how 
+    many areas that actually have rats in the data were also predicted with a high probability of having rats. The second metric
+    describes the precision of the model. It is assesesed by how many areas predicted with a high probability of having rats, 
+    actually turned out to have rats in the data. 
+"""
 
 class WebApp():
     def __init__(self):
+        # Initiliaze properties
         self.clf = None
+        self.precision = None
+        self.recall = None
         self.rat_gdf = None
         self.rat_proba_gdf = None
 
+        # Load PC4 area geometries
+        self.pc_gdf = data_loading.load_pc_geodata(pc_gpkg_path)
+
+        # Mount the directories and prepare the UI
         self.mount_dirs()
         self.build_ui()
         self.reset_map_html()
 
-        self.pc_gdf = data_loading.load_pc_geodata(pc_gpkg_path)
-
 
     def mount_dirs(self):
+        """
+        Creates map and data directoris if they don't exist and mounts them for static files. 
+        Also tries to load saved rat monitor data if it exists in the data directory.
+        """
         if not os.path.exists('map'):
             os.makedirs('map')
         if not os.path.exists('data'):
             os.makedirs('data')
 
+        # Mount static directories
+        # Updating files in these directories won't trigger the webapp to restart
         app.mount("/map", StaticFiles(directory="map"), name="map")
         app.mount("/data", StaticFiles(directory="data"), name="data")
 
-        if os.path.exists(rat_gdf_path) and os.path.exists(rat_proba_gdf_path):
+        # If saved data exists, attempt to load it
+        if os.path.exists(rat_gdf_path) and os.path.exists(rat_proba_gdf_path) and os.path.exists(metrics_path):
             try:
+                # Load rat monitor and prediction data
                 self.rat_gdf = gpd.read_file(rat_gdf_path)
                 self.rat_proba_gdf = gpd.read_file(rat_proba_gdf_path)
-            except:
+
+                # Load mtrics
+                with open(metrics_path, 'r') as file:
+                    metrics = json.load(file)
+                    self.precision = metrics.get('precision')
+                    self.recall = metrics.get('recall')
+
+            except Exception as ex:
                 ui.notify("Error loading data from previous session, please upload the data again.", type='negative')
+                print(ex)
 
 
     def upload_rm_file(self, e: events.UploadEventArguments):
-        # Try to read and transform the uploaded data
-        try:
+        """
+        Event listener for file uploading. 
+
+        Tries to load rat data from the uploaded file.
+        If data loading is succesful, trains the classifier and generates predictions for the upcomig month.
+        Saves the loaded data and the predictions in the data directory.
+        """
+        try:    
+            # Try to read and transform the uploaded data    
             file_bytes = BytesIO(e.content.read())
             rat_df = data_loading.load_rm_data(file_bytes)
             self.rat_gdf = data_loading.transform_to_spatiotemporal_gdf(rat_df, self.pc_gdf)
-            
-        # Raise an exception and notify the user when uploading fails
-        except Exception as e:
-            ui.notify(f'Error processing file: {str(e)}', type='negative')
-            raise
 
-        # Train the classifier and update map with predictions
+        except Exception as ex:   
+            # Print the exception and notify the user when uploading fails
+            ui.notify(f'Error processing file: {str(ex)}', type='negative')
+            print(ex)
+
         else:
-            self.clf = classification.train_classifier(self.rat_gdf)
+            # Train tshe classifier and update map with predictions
+            self.clf, self.precision, self.recall = classification.train_classifier(self.rat_gdf)
             self.rat_proba_gdf = classification.predict_probabilities(self.rat_gdf, self.clf)
 
+            # Save the rat data and predictions for loading on future start ups
             self.rat_gdf.to_file(rat_gdf_path, driver='GPKG')
             self.rat_proba_gdf.to_file(rat_proba_gdf_path, driver='GPKG')
 
+            # Save precision and recall scores to a file as well
+            with open(metrics_path, 'w') as file:
+                json.dump({'precision': float(self.precision), 'recall': float(self.recall)}, file)
+
+            # Reset the ui contents
             self.graph_frame.content = ""
             self.reset_map_html()
 
 
     def update_pc_ui(self, e: events.UiEventArguments):
-        # Retrieve postal code and check if it is valid
+        """
+        Event listener for postal code input.
+
+        If the postal code in the input field is a valid PC4 postal codee, 
+        retrieves and displays historical and prediction data for the postal code.
+        """
         postal_code = e.value.strip()
         
+        # Check if postal code is valid
         if not postal_code:
             return
-        if len(postal_code) < 4 or not postal_code.isnumeric():
+        if len(postal_code) < 3 or not postal_code.isnumeric():
             return
         
-        postal_code = int(postal_code[:4])
+        # Only retrieve the first 4 characters if the input is longer
+        postal_code = int(postal_code[:4]) if len(postal_code) > 4 else int(postal_code)
 
         # Check if rat data is available and if it includes the postal code
         if self.rat_proba_gdf is None:
@@ -120,8 +177,6 @@ class WebApp():
         ax.set_ylim(bottom=0)
         plt.tight_layout()
 
-        print(historic_df)
-
         # Write the plot to an IO buffer and display it in the graph frame
         buf = BytesIO()
         plt.savefig(buf, format='png')
@@ -132,7 +187,7 @@ class WebApp():
 
 
     def reset_map_html(self):
-
+        """Resets the map html element based on the rat monitor data. Creates an empty map if the data is absent."""
         if self.rat_proba_gdf is not None:
             m = mapping.update_map(self.rat_proba_gdf)
         else:
@@ -140,10 +195,11 @@ class WebApp():
         
         m.save(map_html_path)
 
+        # Use a cache buster to force the html element to reload
         cache_buster = random.randint(0, int(1e9))
         self.map_frame.content = f"""
         <div style="position: relative; width: 100%; padding-top: 50%;">
-            <iframe src="./map/map.html?cb={cache_buster}"
+            <iframe src="{map_html_path}?cb={cache_buster}"
                     style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;"
                     loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade">
@@ -151,28 +207,36 @@ class WebApp():
         </div>
         """
 
-        self.upload_label.text = 'Upload Rat Monitor Data:'
-        self.month_label.text = f'Inspecting predictions for {self.rat_proba_gdf['month'].max():%m/%Y}'
+        # Update labels
+        self.upload_label.text = 'Upload new Rat Monitor data:'
+
+        if self.rat_proba_gdf is not None:
+            self.month_label.text = f'Inspecting predictions for {self.rat_proba_gdf['month'].max():%m/%Y}'
+        
+        if self.precision is not None and self.recall is not None:
+            self.precision_label.text = f'{precision_label_text}{self.precision:.1%}'
+            self.recall_label.text = f'{recall_label_text}{self.recall:.1%}'
+
 
 
     def build_ui(self):
-        # Create Grid with 2 columns
+        """Builds the web UI using a grid with 2 rows: a map and an interface."""
         with ui.grid().classes('grid grid-cols-1 md:grid-cols-[3fr_1fr] w-full gap-5'):
-            # Map Card
+
+            # Card for the map
             with ui.card().classes('w-full'):
                 ui.markdown('## Rat Monitor Future Predictions')
                 self.map_frame = ui.html("").classes('w-full')
 
-            # Interface Row
+            # Row for the interface
             with ui.row():
                 with ui.card().classes("w-full"):
                     with ui.column().classes("w-full gap-4"):
 
+                        # Data inspection section
                         with ui.column().classes('w-full'):
-                            if self.rat_proba_gdf is not None:
-                                self.month_label = ui.label('Loading predictions')
-                            else:
-                                self.month_label = ui.label('Upload rat data to inspect predictions')
+                            self.month_label = ui.label('Upload rat data to inspect predictions')
+                            self.month_label.tailwind.font_weight('extrabold')
 
                             self.postal_code_input = ui.input(on_change=self.update_pc_ui, label='Enter postal code', placeholder='e.g. 1234').classes('w-full')
 
@@ -180,10 +244,27 @@ class WebApp():
                             self.proba_label = ui.label('Probability of rats being present: ')
 
                             ui.label('Historical data: ')
-                            self.graph_frame = ui.html("").classes('w-full h-64 rounded bg-white')
+                            self.graph_frame = ui.html("").classes('w-full h-32x rounded bg-white')
 
+                        # Dialog with info about the accuracy metrics
+                        with ui.dialog() as accuracy_dialog, ui.card():
+                            ui.label(accuracy_info_text)
+                            ui.button('Close', on_click=accuracy_dialog.close)
+
+                        # Model performance section
+                        with ui.column().classes('w-full'):
+                            ui.label('Model performance:').tailwind.font_weight('extrabold')
+
+                            self.recall_label = ui.label(recall_label_text)
+                            self.precision_label = ui.label(precision_label_text)
+
+                            ui.button(icon='help_outline', on_click=accuracy_dialog.open).tooltip('What do these metric mean?')
+
+                        # File upload section
                         with ui.column().classes('w-full'):
                             self.upload_label = ui.label('Upload rat monitor data:')
+                            self.upload_label.tailwind.font_weight('extrabold')
+
                             ui.upload(on_upload=self.upload_rm_file).props("accept=.xlsx").classes("max-w-full")
                         
 
